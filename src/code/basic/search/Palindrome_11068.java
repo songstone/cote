2206e1548ecc2f9271f98d0eb37f33b8f5491e2d
Song Seung-hyun <thdtmdgus5505@gmail.com>
2023-05-15 22:23:30 +0900

    code: 11068

---
+++ src/code/basic/search/Palindrome_11068.java
@@ -0,0 +1,36 @@
+package code.basic.search;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Palindrome_11068 {
+    public static void main_11068(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            int result = 0;
+            long chkNum = sc.nextLong();
+            for(int j = 2; j <= 64 ; j++) {
+                String chkString = "";
+                long K = chkNum;
+                while(K > 0) {
+                    chkString += (char)(K % j);
+                    K /= j;
+                }
+                if(isPalindrome(chkString)) {
+                    result = 1;
+                    break;
+                }
+            }
+            System.out.println(result);
+        }
+    }
+
+    public static boolean isPalindrome(String chk) {
+        for (int i = 0, j = chk.length()-1 ; i <= j; i++, j--) {
+            if(chk.charAt(i) != chk.charAt(j)) return false;
+        }
+        return true;
+    }
+}
