fae223464683d1ab67128a18a500cefdba443f36
Song Seung-hyun <thdtmdgus5505@gmail.com>
2023-07-13 18:10:39 +0900

    code: 11728

---
+++ src/code/basic/twoPointer/ArraySum_11728.java
@@ -0,0 +1,52 @@
+package code.basic.twoPointer;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+public class ArraySum_11728 {
+    public static void main_11728(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int[] arrayN = new int[N];
+        int[] arrayM = new int[M];
+        int[] answer = new int[N + M];
+
+        for (int i = 0; i < N; i++) {
+            arrayN[i] = sc.nextInt();
+        }
+        for (int i = 0; i < M; i++) {
+            arrayM[i] = sc.nextInt();
+        }
+
+        int indexN = 0;
+        int indexM = 0;
+        int answerIndex = 0;
+
+        while(indexN < N && indexM < M) {
+            int n = arrayN[indexN];
+            int m = arrayM[indexM];
+            if(n <= m) {
+                answer[answerIndex++] = n;
+                indexN++;
+            } else {
+                answer[answerIndex++] = m;
+                indexM++;
+            }
+        }
+
+        while(indexN < N) answer[answerIndex++] = arrayN[indexN++];
+        while(indexM < M) answer[answerIndex++] = arrayM[indexM++];
+
+        for (int i : answer) {
+            bw.write(i + " ");
+        }
+        bw.write("\n");
+        bw.flush();
+    }
+}
