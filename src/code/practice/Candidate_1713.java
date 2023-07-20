32dbf20cd4ae38080e45b8a377320cd58d0f59a9
Song Seung-hyun <thdtmdgus5505@gmail.com>
2023-07-20 23:19:05 +0900

    code: 1713

---
+++ src/code/practice/Candidate_1713.java
@@ -0,0 +1,52 @@
+package code.practice;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Candidate_1713 {
+    public static void main_1713 (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int R = sc.nextInt();
+        List<Student> post = new ArrayList<>();
+        for (int i = 0; i < R; i++) {
+            int votedNumber = sc.nextInt();
+            boolean alreadyPosted = false;
+            for (int j = 0; j < post.size(); j++)
+                if (post.get(j).number == votedNumber) {
+                    post.get(j).scr++;
+                    alreadyPosted = true;
+                    break;
+                }
+
+            if (!alreadyPosted) {
+                if (post.size() < N) post.add(0, new Student(votedNumber, 1, i));
+                else {
+                    Collections.sort(post, (o1, o2) ->
+                        o1.scr == o2.scr ? o1.postedAt - o2.postedAt : o1.scr - o2.scr);
+                    post.set(0, new Student(votedNumber, 1, i));
+                }
+            }
+        }
+
+        Collections.sort(post, (o1, o2) -> o1.number - o2.number);
+        System.out.println(post.stream()
+            .map(student -> String.valueOf(student.number))
+            .collect(Collectors.joining(" ")));
+    }
+}
+
+class Student {
+    int number;
+    int scr;
+    int postedAt;
+    Student(int number, int scr, int postedAt) {
+        this.number = number;
+        this.scr = scr;
+        this.postedAt = postedAt;
+    }
+}
