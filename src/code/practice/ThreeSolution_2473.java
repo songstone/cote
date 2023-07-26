6e9dd962d0bd695147e62e36f3a5bad31e990c3e
Song Seung-hyun <thdtmdgus5505@gmail.com>
2023-07-26 12:03:51 +0900

    code: 2473 update

--- src/code/practice/ThreeSolution_2473.java
+++ src/code/practice/ThreeSolution_2473.java
@@ -1,40 +1,44 @@
 package code.practice;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class ThreeSolution_2473 {
-    public static void main_2473 (String[] args) {
+    public static void main_2473(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++)
-            arr[i] = sc.nextInt();
+        int[] solutions = new int[N];
+        for (int i = 0; i < N; i++) {
+            solutions[i] = sc.nextInt();
+        }
 
-        Arrays.sort(arr);
+        Arrays.sort(solutions);
 
-        int ans1 = arr[0];
-        int ans2 = arr[1];
-        int ans3 = arr[2];
-        long ansAbs = Math.abs((long)arr[0] + arr[1] + arr[2]);
+        int answer1 = solutions[0];
+        int answer2 = solutions[1];
+        int answer3 = solutions[2];
+
+        long sum = Math.abs((long)answer1 + answer2 + answer3);
+
         for (int i = 0; i < N - 2; i++) {
-            int leftIndex = i + 1;
-            int rightIndex = N - 1;
-            while (leftIndex < rightIndex) {
-                long currentSum = (long)arr[i] + arr[leftIndex] + arr[rightIndex];
-                long currentAbs = Math.abs(currentSum);
-                if (ansAbs > currentAbs) {
-                    ans1 = arr[i];
-                    ans2 = arr[leftIndex];
-                    ans3 = arr[rightIndex];
-                    ansAbs = currentAbs;
+            int startIndex = i+1;
+            int endIndex = N-1;
+            while (startIndex < endIndex) {
+                long first = (long) solutions[i];
+                long chkSum = first + solutions[startIndex] + solutions[endIndex];
+                if(Math.abs(chkSum) < sum) {
+                    answer1 = solutions[i];
+                    answer2 = solutions[startIndex];
+                    answer3 = solutions[endIndex];
+                    sum = Math.abs(chkSum);
                 }
-                if (currentSum <= 0) leftIndex++;
-                else rightIndex--;
+                if(chkSum < 0) startIndex++;
+                else endIndex--;
             }
         }
 
-        System.out.println(ans1 + " " + ans2 + " " + ans3);
+        System.out.println(answer1 + " " + answer2 + " " + answer3);
     }
 }
