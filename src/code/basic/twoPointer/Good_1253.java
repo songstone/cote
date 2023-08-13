91cb56d7cfbf10298959e381089623482a0ee6e3
Song Seung-hyun <thdtmdgus5505@gmail.com>
2023-08-13 20:07:26 +0900

    code: 1253

---
+++ src/code/basic/twoPointer/Good_1253.java
@@ -0,0 +1,49 @@
+package code.basic.twoPointer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Good_1253 {
+    public static void main_1253(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] nums = new int[N];
+        String[] inputs = br.readLine().split(" ");
+
+        for (int i = 0; i < N; i++) {
+            nums[i] = Integer.parseInt(inputs[i]);
+        }
+
+        Arrays.sort(nums);
+
+        int answer = 0;
+        for (int i = N-1; i >= 0; i--) {
+            int leftIndex = 0;
+            int rightIndex = N-1;
+            while(leftIndex < rightIndex) {
+                int chkSum = nums[leftIndex] + nums[rightIndex];
+                if(chkSum == nums[i]) {
+                    if(leftIndex == i) {
+                        leftIndex++;
+                        continue;
+                    }
+                    if(rightIndex == i) {
+                        rightIndex--;
+                        continue;
+                    }
+                    answer++;
+                    break;
+                } else if(chkSum > nums[i]) {
+                    rightIndex--;
+                } else {
+                    leftIndex++;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
