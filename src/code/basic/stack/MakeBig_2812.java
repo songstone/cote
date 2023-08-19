5891d39d58b4dff4650b979091d96f09271736ac
Song Seung-hyun <thdtmdgus5505@gmail.com>
2023-08-19 22:49:09 +0900

    code: 2812

---
+++ src/code/basic/stack/MakeBig_2812.java
@@ -0,0 +1,25 @@
+package code.basic.stack;
+
+import java.util.Scanner;
+
+public class MakeBig_2812 {
+    public static void main_2812(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        char[] digits = sc.next().toCharArray();
+
+        int ansLength = N - K;
+        char[] maxDigits = new char[N];
+        int topIndex = -1;
+        for (char d : digits) {
+            while (K > 0 && topIndex >= 0 && maxDigits[topIndex] < d) {
+                topIndex--;
+                K--;
+            }
+            maxDigits[++topIndex] = d;
+        }
+        System.out.println(new String(maxDigits, 0, ansLength));
+    }
+}
