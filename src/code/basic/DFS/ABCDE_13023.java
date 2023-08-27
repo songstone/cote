54c810128927c57ab286d3835f95c6624bfde9ec
Song Seung-hyun <thdtmdgus5505@gmail.com>
2023-08-27 20:01:33 +0900

    code:13023

---
+++ src/code/basic/DFS/ABCDE_13023.java
@@ -0,0 +1,49 @@
+package code.basic.DFS;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ABCDE_13023 {
+    static boolean[] visited;
+    static ArrayList<Integer>[] graph;
+    static boolean find = false;
+
+    public static void main_13023(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        visited = new boolean[N];
+        graph = new ArrayList[N];
+        for (int i = 0; i < N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        while (M-- > 0) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            graph[A].add(B);
+            graph[B].add(A);
+        }
+
+        for (int i = 0; i < N; i++) {
+            findRelation(i, 1);
+            if(find) break;
+        }
+
+        System.out.println(find ? 1 : 0);
+    }
+
+    public static void findRelation(int num, int depth) {
+        if(depth == 5) {
+            find = true;
+            return;
+        }
+
+        visited[num] = true;
+        for (int i : graph[num]) {
+            if(!visited[i]) findRelation(i, depth + 1);
+        }
+        visited[num] = false;
+    }
+}
