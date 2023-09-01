cbd90dda4c894fb6029bda109d974ba207f0c342
Song Seung-hyun <thdtmdgus5505@gmail.com>
2023-09-01 23:35:55 +0900

    code: 2343 resolve

--- src/code/basic/binarySearch/GuitarLesson_2343.java
+++ src/code/basic/binarySearch/GuitarLesson_2343.java
@@ -1,40 +1,46 @@
 package code.basic.binarySearch;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class GuitarLesson_2343 {
-    public static boolean isPossible(int[] lengths, int videoLength, int videoCount) {
-        int currentLength = 0;
-        int currentCount = 1;
-        for (int len : lengths) {
-            if (len > videoLength) return false;
-            if (currentLength + len > videoLength) {
-                if (++currentCount > videoCount) return false;
-                currentLength = 0;
-            }
-            currentLength += len;
+    static int limitCnt;
+    public static void main_2343(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        limitCnt = sc.nextInt();
+
+        int[] lectures = new int[N];
+        for (int i = 0; i < N; i++) {
+            lectures[i] = sc.nextInt();
         }
-        return true;
+
+        int startLength = 1;
+        int endLength = N*10000;
+        int answer = 0;
+        while(startLength <= endLength) {
+            int length = (startLength + endLength) / 2;
+            if(chkLength(lectures, length)) {
+                endLength = length - 1;
+                answer = length;
+            } else {
+                startLength = length + 1;
+            }
+        }
+        System.out.println(answer);
     }
 
-    public static void main_2343 (String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int[] lengths = new int[N];
-        for (int i = 0; i < N; i++)
-            lengths[i] = sc.nextInt();
-
-        int l = 1, r = N * 10000, ans = -1;
-        while (l <= r) {
-            int m = (l + r) / 2;
-            if (isPossible(lengths, m, M)) {
-                ans = m;
-                r = m - 1;
+    public static boolean chkLength(int[] lectures, int length) {
+        int chkSum = 0;
+        int bluRayCnt = 1;
+        for (int lecture : lectures) {
+            if(lecture > length) return false;
+            if(chkSum + lecture > length) {
+                if(++bluRayCnt > limitCnt) return false;
+                chkSum = 0;
             }
-            else l = m + 1;
+            chkSum += lecture;
         }
-        System.out.println(ans);
+        return true;
     }
 }
