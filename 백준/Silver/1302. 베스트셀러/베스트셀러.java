2835218fc633de3b50089243c60e8ab16834377e
songstone <87690871+songstone@users.noreply.github.com>
2023-09-05 19:31:08 +0900

    [Silver IV] Title: 베스트셀러, Time: 120 ms, Memory: 12908 KB -BaekjoonHub

---
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		String[] sellLog = new String[N];
+		
+		for (int i = 0; i < N; i++) {
+			sellLog[i] = sc.next();
+		}
+		
+		Arrays.sort(sellLog);
+		
+		String bestSeller = sellLog[0];
+		int maxSellCount = 1;
+		int sellCount = 1;
+		
+		for (int i = 1; i < sellLog.length; i++) {
+			if(!sellLog[i-1].equals(sellLog[i])) sellCount = 0;
+			sellCount++;
+			if(sellCount > maxSellCount) {
+				bestSeller = sellLog[i];
+				maxSellCount = sellCount;
+			}
+		}
+		
+		System.out.println(bestSeller);
+	}
+
+}
