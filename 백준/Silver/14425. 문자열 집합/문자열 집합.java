2470b72cedd566f487be388ed4f12e3edd17bb51
songstone <87690871+songstone@users.noreply.github.com>
2023-09-27 15:12:14 +0900

    [Silver III] Title: 문자열 집합, Time: 904 ms, Memory: 67988 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int T = sc.nextInt();
+
+        String[] inputs = new String[N];
+        for (int i = 0; i < N; i++) {
+            inputs[i] = sc.next();
+        }
+        Arrays.sort(inputs);
+
+        int answer = 0;
+        while(T-- > 0) {
+            if(isExist(inputs, sc.next())) answer++;
+        }
+
+        System.out.println(answer);
+    }
+
+    public static boolean isExist(String[] arr, String key) {
+        int startIndex = 0;
+        int endIndex = arr.length - 1;
+
+        while(startIndex <= endIndex) {
+            int chkIndex = (startIndex + endIndex) / 2;
+            if(arr[chkIndex].compareTo(key) > 0) endIndex = chkIndex -1;
+            else if(arr[chkIndex].compareTo(key) < 0) startIndex = chkIndex + 1;
+            else return true;
+        }
+
+        return false;
+    }
+}
