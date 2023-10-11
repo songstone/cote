1855c3fd498d9bd9dd38ff7101f71f2a8fa48a75
songstone <87690871+songstone@users.noreply.github.com>
2023-10-11 19:11:10 +0900

    [Silver V] Title: 셀프 넘버, Time: 124 ms, Memory: 12268 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean[] existArray = new boolean[10001];
+        int i = 0;
+        while (++i < 10000) {
+            if(existArray[i]) {
+                continue;
+            }
+            int num = i;
+            while(num <= 10000) {
+                num = num + sumNum(num);
+                if(num > 10000 || existArray[num]) break;
+                existArray[num] = true;
+            }
+        }
+
+        for (int j = 1; j < existArray.length; j++) {
+            if(!existArray[j]) System.out.println(j);
+        }
+    }
+
+    public static int sumNum(int number) {
+        int sum = 0;
+        while (number != 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+        return sum;
+    }
+}
